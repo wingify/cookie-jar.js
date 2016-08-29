c8bd145ea648043e5727643c609762de63580d44
Himanshu Kapoor <himanshu.kapoor@wingify.com>
2016-08-29 12:27:22 +0530

    Update

--- src/cookie-jar.js
+++ src/cookie-jar.js
@@ -1,301 +1,284 @@
-(function (window, document) {
-	'use strict';
+'use strict'
 
-	var encode = window.encodeURIComponent;
-	var decode = window.decodeURIComponent;
+var encode = window.encodeURIComponent
+var decode = window.decodeURIComponent
+var reUppercase = /[A-Z]/
+var errors = {
+	COOKIE_TOO_LARGE: 'ctl',
+	COOOKIE_LIMIT_REACHED: 'clr'
+}
 
-	/* jshint bitwise: false */
+function error(s) {
+	throw new Error(s)
+}
+
+
+
+/**
+ * Gets a cookie (native)
+ *
+ * @param  {String} name The cookie key to get it by
+ * @return {String}      Value of the cookie if it exists. Undefined otherwise.
+ */
+function getCookie(name) {
+	var match = document.cookie.match(new RegExp('(?:^|;)\\s?' + name + '=(.*?)(?:;|$)', 'i'))
+	return match && decode(match[1])
+}
+
+/**
+ * Sets a cookie (native)
+ *
+ * @param {String} name     Cookie key
+ * @param {String} value    Value of the cookie
+ * @param {String} [domain] Optional domain to set the cookie for
+ */
+function setCookie(name, value, domain) {
+	name = encode(name)
+	value = encode(value)
+	var c = name + '=' + value + '; '
+	if (domain) c += 'domain=' + domain + '; '
+
+	c += 'expires=Sun, 26 Jul 2116 11:47:19 GMT; path=/'
+
+	document.cookie = c
+
+	if (getCookie(name) !== value)
+		error(errors.COOOKIE_LIMIT_REACHED)
+}
+
+
+
+/**
+ * Converts a character to a number between 0 - 63.
+ *
+ * @param  {String} s A character between 0 - 9, a - z, A - Z, _ and -
+ * @return {Number}   An integer between 0 - 63
+ */
+function charToNumber(s) {
+	var n = reUppercase.test(s) * 26 + parseInt(s, 36)
+
+	if (s === '_') n = 62
+	if (s === '-') n = 63
+
+	return n
+}
+
+/**
+ * Converts a number between 0 - 63 into a character.
+ *
+ * @param  {Number} n An integer between 0 - 63
+ * @return {String}   A character between 0 - 9, a - z, A - Z, _ and -
+ */
+function numberToChar(n) {
+	var s = n.toString(36)
+
+	if (n >= 36) s = String.fromCharCode(n + 29)
+	if (n === 62) s = '_'
+	if (n === 63) s = '-'
+
+	return s
+}
+
+/**
+ * Encodes a number into a set of base 64 characters.
+ *
+ * @param  {Number} n    The number to encode
+ * @param  {Number} padl Number of digits the output should be
+ *                       (0 padding will be added if it isn't).
+ * @return {String}      A string representation of the number.
+ */
+function encodeNumber(n, padl) {
+	var n2 = ''
+	while (n) {
+		n2 = numberToChar(n % 64) + n2
+		n = Math.floor(n / 64)
+	}
+	n = n2 || (n + '')
+	if (n.length < padl) n = '0' + n
+	return n
+}
+
+/**
+ * Decodes a number from base 64 to decimal.
+ *
+ * @param  {String} n A base 64 representation of the number.
+ * @return {Number}   The integer in decoded form.
+ */
+function decodeNumber(n) {
+	var n2 = 0, j = 0
+	while (n) {
+		n2 += charToNumber(n.slice(-1)) * Math.pow(64, j++)
+		n = n.slice(0, -1)
+	}
+	return n2
+}
+
+/**
+ * Encodes a cookie into a single string representing its key, value, expiry
+ * and additional information.
+ *
+ * @param  {String} key     Cookie key
+ * @param  {String} value   Cookie value
+ * @param  {Number} expiry  Expiry of cookie in days (min - 0.01, max - 2047)
+ * @return {String}         Cookie in encoded form
+ */
+function encodeCookie(key, value, expiry) {
+	expiry = expiry || 0
+	if (key.length >= 16 || value.length >= 256 || expiry >= 2048) error(errors.COOKIE_TOO_LARGE)
+	var microcookie = key.length < 8 && value.length < 8
+	var smallExpiry = expiry < 1
+	expiry = Math.floor(expiry * (smallExpiry ? 100 : 1))
+	return (microcookie ? '' : '!') +
+		encodeNumber((key.length << 0) + (value.length << (microcookie ? 3 : 4)), !microcookie + 1) +
+		encodeNumber((smallExpiry << 0) + (expiry << 1), 2) +
+		key + value
+}
+
+/**
+ * Decodes a cookie from encoded cookie string
+ * @param  {String} cookie Cookie encoded string
+ * @param  {Number} rel    A timestamp (in ms) to consider the relative expiry from.
+ * @return {Array}         Returns an array containing 4 items
+ *                         0 - cookie key
+ *                         1 - cookie value
+ *                         2 - cookie expiry timestamp (in ms)
+ *                         3 - cookie length (in encoded form)
+ */
+function decodeCookie(cookie, rel) {
+	var microcookie = cookie.charAt(0) !== '!'
+	var t = decodeNumber(cookie.substr(+!microcookie, !microcookie + 1))
+	var keyLength, valueLength, cookieLength = !microcookie * 2 + 1
+	cookie = cookie.substr(!microcookie * 2 + 1)
+	if (microcookie) {
+		keyLength = t >> 0 & 7
+		valueLength = t >> 3 & 7
+	} else {
+		keyLength = t >> 0 & 15
+		valueLength = t >> 4 & 255
+	}
+	var expiry = decodeNumber(cookie.substr(0, 2))
+	var smallExpiry = expiry >> 0 & 1
+	expiry = (expiry >> 1 & 2047) / (smallExpiry ? 100 : 1)
+	cookieLength += 2 + keyLength + valueLength
+	return [
+		cookie.substr(2, keyLength), // key
+		cookie.substr(2 + keyLength, valueLength), // value
+		expiry ? rel + expiry * 864e5 : 0, // expiry
+		cookieLength // length
+	]
+}
+
+/**
+ * Checks whether a cookie is expired
+ *
+ * @param  {Array}   cookie Cookie array object containing its key, value,
+ *                          expiry timestamp and encoded length.
+ * @return {Boolean}        Boolean indicating whether its expired or not.
+ */
+function isCookieExpired(cookie) {
+	return !cookie || cookie[2] && Date.now() > cookie[2]
+}
+
+// @private
+function convertExpiry(expiry, rel) {
+	return Math.floor(expiry + (Date.now() - rel) / 864e5)
+}
+
+
+/**
+ * Creates a cookie jar object.
+ *
+ * @param {String} jarName  Name of cookie jar cookie name.
+ * @param {String} [domain] Optional domain name to set the jar cookie on.
+ */
+function CookieJar(jarName, domain) {
+	function cookieForEach(cookies, fn) {
+		var cookie
+		var l = cookies.length, i = 0
+		while (i < l) {
+			cookie = decodeCookie(cookies, rel)
+			if (fn(cookie, i)) { return }
+			i += cookie[3] // length
+			cookies = cookies.substr(cookie[3])
+		}
+	}
+
+	// expires a cookie by removing it from the jar.
+	function expireCookie(cookie, i) {
+		cookieString = cookieString.substr(0, i) + cookieString.substr(i + cookie[3] /* length */)
+		delete cache[cookie[0]]
+		sync()
+		return true
+	}
+
+	/// gets a cookie in the jar by a key
+	function get(key) {
+		if (!isCookieExpired(cache[key])) { return cache[key][1] }
+
+		var val
+		cookieForEach(cookieString, function (cookie, i) {
+			cache[cookie[0]] = cookie
+			if (key !== cookie[0]) return
+			if (isCookieExpired(cookie)) expireCookie(cookie, i)
+			val = cookie[1]
+			return true
+		})
+		return val
+	}
+
+	/// sets a cookie in the jar with an expiry
+	function set(key, value, expiry) {
+		unset(key)
+		var encodedCookie = encodeCookie(key, value, convertExpiry(expiry, rel))
+		cookieString += encodedCookie
+		var cookie = decodeCookie(encodedCookie)
+		cache[cookie[0]] = cookie
+		sync()
+		return value
+	}
+
+	/// unsets a cookie by name by removing it from the jar
+	function unset(key) {
+		cookieForEach(cookieString, function (cookie, i) {
+			if (key === cookie[0]) expireCookie(cookie, i)
+		})
+	}
+
+	/// syncs the jar cookies with document.cookie
+	function sync() {
+		setCookie(jarName, cookieString, domain)
+	}
 
 	/**
-	 * Gets a cookie (native)
+	 *  gets all the cookies in the jar
 	 *
-	 * @param  {String} name The cookie key to get it by
-	 * @return {String}      Value of the cookie if it exists. Undefined otherwise.
+	 * @return {Object} A key-value pair where key is the cookie key
+	 *                  and value is an array of two items where
+	 *                  0 - cookie value
+	 *                  1 - cookie expiry timestamp (in ms)
 	 */
-	function getCookie(name) {
-		var match = document.cookie.match(new RegExp('(?:^|;)\\s?' + name + '=(.*?)(?:;|$)', 'i'));
-		return match && decode(match[1]);
+	function getAll() {
+		var cookies = {}
+		cookieForEach(cookieString, function (cookie, i) {
+			if (isCookieExpired(cookie)) expireCookie(cookie, i)
+			cookies[cookie[0]] = [cookie[1], cookie[2] && new Date(cookie[2])]
+		})
+		return cookies
 	}
 
-	/**
-	 * Sets a cookie (native)
-	 *
-	 * @param {String} name     Cookie key
-	 * @param {String} value    Value of the cookie
-	 * @param {String} [domain] Optional domain to set the cookie for
-	 */
-	function setCookie(name, value, domain) {
-		var c = encode(name) + '=' + encode(value) + '; ';
-		if (domain) {
-			c += 'domain=' + domain + '; ';
-		}
-		c += 'expires=Sun, 26 Jul 2116 11:47:19 GMT; path=/';
+	var cookieString = getCookie(jarName) || ''
+	var cache = {}
+	var rel = get('_')
+	if (!rel) rel = set('_', encodeNumber(Date.now()))
 
-		document.cookie = c;
+	rel = decodeNumber(rel)
+
+	return {
+		get: get,
+		set: set,
+		unset: unset,
+		getAll: getAll
 	}
+}
 
-	var parseInt = window.parseInt;
-	var reUppercase = /[A-Z]/;
-
-	/**
-	 * Converts a character to a number between 0 - 63.
-	 *
-	 * @param  {String} s A character between 0 - 9, a - z, A - Z, _ and -
-	 * @return {Number}   An integer between 0 - 63
-	 */
-	function charToNumber(s) {
-		var n = reUppercase.test(s) * 26 + parseInt(s, 36);
-
-		if (s === '_') { n = 62; }
-		if (s === '-') { n = 63; }
-
-		return n;
-	}
-
-	/**
-	 * Converts a number between 0 - 63 into a character.
-	 *
-	 * @param  {Number} n An integer between 0 - 63
-	 * @return {String}   A character between 0 - 9, a - z, A - Z, _ and -
-	 */
-	function numberToChar(n) {
-		var s = n.toString(36);
-
-		if (n >= 36) { s = String.fromCharCode(n + 29); }
-		if (n === 62) { s = '_'; }
-		if (n === 63) { s = '-'; }
-
-		return s;
-	}
-
-	/**
-	 * Converts an integer from decimal to a particular base
-	 *
-	 * @param  {Number} n    An integer to shift base of.
-	 * @param  {Number} base An integer between 1 - 64
-	 * @return {String}      A string representation of the number in that
-	 *                       particular base's number system.
-	 */
-	function decimalToBase(n, base) {
-		var n2 = '';
-		while (n) {
-			n2 = numberToChar(n % base) + n2;
-			n = Math.floor(n / base);
-		}
-		return n2 || n.toString();
-	}
-
-	/**
-	 * Converts a string representation of a number in a particular base to decimal
-	 * (base 10)
-	 *
-	 * @param  {String} n    A character between 0 - 9, a - z, A - Z, _ and -
-	 * @param  {Number} base Base to parse the string in
-	 * @return {Number}      An integer representing the given number in decimal.
-	 */
-	function baseToDecimal(n, base) {
-		var n2 = 0, j = 0;
-		while (n) {
-			n2 += charToNumber(n.slice(-1)) * Math.pow(base, j++);
-			n = n.slice(0, -1);
-		}
-		return n2;
-	}
-
-	/**
-	 * Encodes a number into a set of base 64 characters.
-	 *
-	 * @param  {Number} n    The number to encode
-	 * @param  {Number} padl Number of digits the output should be
-	 *                       (0 padding will be added if it isn't).
-	 * @return {String}      A string representation of the number.
-	 */
-	function encodeNumber(n, padl) {
-		n = decimalToBase(n, 64);
-		if (n.length < padl) { n = '0' + n; }
-		return n;
-	}
-
-	/**
-	 * Decodes a number from base 64 to decimal.
-	 *
-	 * @param  {String} n A base 64 representation of the number.
-	 * @return {Number}   The integer in decoded form.
-	 */
-	function decodeNumber(n) {
-		return baseToDecimal(n, 64);
-	}
-
-	/**
-	 * Encodes a cookie into a single string representing its key, value, expiry
-	 * and additional information.
-	 *
-	 * @param  {String} key     Cookie key
-	 * @param  {String} value   Cookie value
-	 * @param  {Number} expires Expiry of cookie in days (min - 0.01, max - 2047)
-	 * @return {String}         Cookie in encoded form
-	 */
-	function encodeCookie(key, value, expires) {
-		var microcookie = key.length < 8 && value.length < 8;
-		expires = expires || 0;
-		var smallExpiry = expires < 1;
-		if (smallExpiry) {
-			expires = Math.floor(expires * 100);
-		}
-		return (microcookie ? '' : '!') +
-			encodeNumber((key.length << 0) + (value.length << (microcookie ? 3 : 4))) +
-			encodeNumber((smallExpiry << 0) + (expires << 1), 2) +
-			key + value;
-	}
-
-	/**
-	 * Decodes a cookie from encoded cookie string
-	 * @param  {String} cookie Cookie encoded string
-	 * @param  {Number} rel    A timestamp (in ms) to consider the relative expiry from.
-	 * @return {Array}         Returns an array containing 4 items
-	 *                         0 - cookie key
-	 *                         1 - cookie value
-	 *                         2 - cookie expiry timestamp (in ms)
-	 *                         3 - cookie length (in encoded form)
-	 */
-	function decodeCookie(cookie, rel) {
-		var microcookie = cookie.charAt(0) !== '!';
-		var t = decodeNumber(cookie.substr(+!microcookie, !microcookie + 1));
-		var keyLength, valueLength, cookieLength = !microcookie * 2 + 1;
-		cookie = cookie.substr(!microcookie * 2 + 1);
-		if (microcookie) {
-			keyLength = t >> 0 & 7;
-			valueLength = t >> 3 & 7;
-		} else {
-			keyLength = t >> 0 & 15;
-			valueLength = t >> 4 & 255;
-		}
-		var expiry = decodeNumber(cookie.substr(0, 2));
-		var smallExpiry = expires >> 0 & 1;
-		expiry = (expires >> 1 & 2047) / (smallExpiry * 100);
-		cookieLength += 2 + keyLength + valueLength;
-		return [
-			cookie.substr(2, keyLength), // key
-			cookie.substr(2 + keyLength, valueLength), // value
-			expiry ? rel + expiry * 864e5 : 0, // expiry
-			cookieLength // length
-		];
-	}
-
-	/**
-	 * Checks whether a cookie is expired
-	 *
-	 * @param  {Array}   cookie Cookie array object containing its key, value,
-	 *                          expiry timestamp and encoded length.
-	 * @return {Boolean}        Boolean indicating whether its expired or not.
-	 */
-	function isCookieExpired(cookie) {
-		return !cookie || cookie[2] && Date.now() > cookie[2];
-	}
-
-	// @private
-	function convertExpiry(expiry, rel) {
-		return Math.floor(expiry + (Date.now() - rel) / 864e5);
-	}
-
-
-	/**
-	 * Creates a cookie jar object.
-	 *
-	 * @param {String} jarName  Name of cookie jar cookie name.
-	 * @param {String} [domain] Optional domain name to set the jar cookie on.
-	 */
-	function CookieJar(jarName, domain) {
-		function cookieForEach(cookies, fn) {
-			var cookie;
-			var l = cookies.length, i = 0;
-			while (i < l) {
-				cookie = decodeCookie(cookies, rel);
-				if (fn(cookie, i)) { return; }
-				i += cookie[3]; // length
-				cookies = cookies.substr(cookie[3]);
-			}
-		}
-
-		// expires a cookie by removing it from the jar.
-		function expireCookie(cookie, i) {
-			cookieString = cookieString.substr(0, i) + cookieString.substr(i + cookie[3] /* length */);
-			delete cache[cookie[0]];
-			sync();
-			return true;
-		}
-
-		/// gets a cookie in the jar by a key
-		function get(key) {
-			if (!isCookieExpired(cache[key])) { return cache[key][1]; }
-
-			var val;
-			cookieForEach(cookieString, function (cookie, i) {
-				cache[cookie[0]] = cookie;
-				if (key !== cookie[0]) { return; }
-				if (isCookieExpired(cookie)) { expireCookie(cookie, i); }
-				val = cookie[1];
-				return true;
-			});
-			return val;
-		}
-
-		/// sets a cookie in the jar with an expiry
-		function set(key, value, expires) {
-			unset(key);
-			var encodedCookie = encodeCookie(key, value, convertExpiry(expires, rel));
-			cookieString += encodeCookie;
-			var cookie = decodeCookie(encodedCookie);
-			cache[cookie[0]] = cookie;
-			sync();
-			return value;
-		}
-
-		/// unsets a cookie by name by removing it from the jar
-		function unset(key) {
-			cookieForEach(cookieString, function (cookie, i) {
-				if (key === cookie[0]) { expireCookie(cookie, i); }
-			});
-		}
-
-		/// syncs the jar cookies with document.cookie
-		function sync() {
-			setCookie(jarName, cookieString, domain);
-		}
-
-		/**
-		 *  gets all the cookies in the jar
-		 *
-		 * @return {Object} A key-value pair where key is the cookie key
-		 *                  and value is an array of two items where
-		 *                  0 - cookie value
-		 *                  1 - cookie expiry timestamp (in ms)
-		 */
-		function getAll() {
-			var cookies = {};
-			cookieForEach(cookieString, function (cookie, i) {
-				if (isCookieExpired(cookie)) { expireCookie(cookie, i); }
-				cookies[cookie[0]] = [cookies[1], new Date(cookies[2])];
-			});
-			return cookies;
-		}
-
-		var cookieString = getCookie(jarName) || '';
-		var cache = {};
-		var rel = get('_');
-		if (!rel) {
-			rel = set('_', encodeNumber(Date.now()));
-		}
-
-		rel = decodeNumber(rel);
-
-		return {
-			get: get,
-			set: set,
-			unset: unset,
-			getAll: getAll
-		};
-	}
-
-	return CookieJar;
-})(this, document);
+if (typeof exports !== 'undefined') exports = CookieJar
---
+++ test/cookie-jar.spec.js
@@ -0,0 +1,128 @@
+describe('class: CookieJar', function () {
+	describe('constructor', function () {
+		it('should work', function () {
+			expect(true).toBeTruthy()
+		})
+	})
+})
+
+var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-'
+
+describe('method: charToNumber', function () {
+	it('converts a character [0-9a-zA-Z_-] to a number between 0 - 63', function () {
+		for (var i = 0; i < chars.length; i++) {
+			expect(charToNumber(chars.charAt(i))).toBe(i)
+		}
+	})
+})
+
+describe('method: numberToChar', function () {
+	it('converts a number between 0 - 63 to a character [0-9a-zA-Z_-]', function () {
+		for (var i = 0; i < chars.length; i++) {
+			expect(numberToChar(i)).toBe(chars.charAt(i))
+		}
+	})
+})
+
+describe('method: encodeNumber', function () {
+	it('converts an integer to base 64', function () {
+		expect(encodeNumber(188)).toBe('2Y')
+		expect(encodeNumber(368)).toBe('5M')
+		expect(encodeNumber(755)).toBe('bP')
+		expect(encodeNumber(1023)).toBe('f-')
+		expect(encodeNumber(2252)).toBe('zc')
+		expect(encodeNumber(10003)).toBe('2sj')
+		expect(encodeNumber(349555)).toBe('1llP')
+		expect(encodeNumber(4663622)).toBe('hOB6')
+		expect(encodeNumber(+new Date(2009, 3, 3))).toBe('i1E63N0')
+	})
+})
+
+describe('method: decodeNumber', function () {
+	it('converts a base 64 representation of a decimal number into decimal', function () {
+		expect(decodeNumber('2Y')).toBe(188)
+		expect(decodeNumber('5M')).toBe(368)
+		expect(decodeNumber('bP')).toBe(755)
+		expect(decodeNumber('f-')).toBe(1023)
+		expect(decodeNumber('zc')).toBe(2252)
+		expect(decodeNumber('2sj')).toBe(10003)
+		expect(decodeNumber('1llP')).toBe(349555)
+		expect(decodeNumber('hOB6')).toBe(4663622)
+		expect(decodeNumber('i1E63N0')).toBe(+new Date(2009, 3, 3))
+	})
+})
+
+describe('method: encodeCookie', function () {
+	it('encodes a cookie key, value and expiry into a single string', function () {
+		// small key, small value, no expiry
+		expect(encodeCookie('a', 'b')).toBe('901ab')
+		// small key, small value, < 1 expiry
+		expect(encodeCookie('a', 'b', 0.34)).toBe('915ab')
+		// small key, small value, has expiry
+		expect(encodeCookie('a', 'b', 254)).toBe('97Yab')
+		// small key, small value, large expiry
+		expect(encodeCookie('a', 'b', 2040)).toBe('9-Mab')
+
+		// big key, small value, no expiry
+		expect(encodeCookie('8 chars or more', 'b')).toBe('!0v018 chars or moreb')
+		// big key, small value, < 1 expiry
+		expect(encodeCookie('8 chars or more', 'b', 0.33)).toBe('!0v138 chars or moreb')
+		// big key, small value, has expiry
+		expect(encodeCookie('8 chars or more', 'b', 256)).toBe('!0v808 chars or moreb')
+		// big key, small value, large expiry
+		expect(encodeCookie('8 chars or more', 'b', 2040)).toBe('!0v-M8 chars or moreb')
+
+		// big key, big value, no expiry
+		expect(encodeCookie('8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.')).toBe('!rf018 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.')
+		// big key, big value, <1 expiry
+		expect(encodeCookie('8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 0.22)).toBe('!rf0J8 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.')
+		// big key, big value, has expiry
+		expect(encodeCookie('8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 446)).toBe('!rfdY8 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.')
+		// big key, big value, large expiry
+		expect(encodeCookie('8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 2030)).toBe('!rf-s8 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.')
+	})
+	it('throws an error if cookie is too large', function () {
+		// large key (> 15 chars)
+		expect(function () {
+			encodeCookie('khojt78t98hihojhg989889', 's')
+		}).toThrow()
+
+		// large value (> 255 chars)
+		expect(function () {
+			encodeCookie('kkey', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz. A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz. A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.')
+		}).toThrow()
+
+		// not large expiry, no throw
+		expect(function () {
+			encodeCookie('key', 'value', 2047)
+		}).not.toThrow()
+
+		// large expiry throws
+		expect(function () {
+			encodeCookie('key', 'value', 2048)
+		}).toThrow()
+		expect(function () {
+			encodeCookie('key', 'value', 20228)
+		}).toThrow()
+	})
+})
+
+
+describe('method: decodeCookie', function () {
+	it('decodes a cookie in encoded form and gives back an array of results', function () {
+		var rel = +new Date(2016, 5, 26)
+
+		expect(decodeCookie('901ab', rel)).toEqual([ 'a', 'b', 0, 5 ])
+		expect(decodeCookie('915ab', rel)).toEqual([ 'a', 'b', 1466908776000, 5 ])
+		expect(decodeCookie('97Yab', rel)).toEqual([ 'a', 'b', 1488825000000, 5 ])
+		expect(decodeCookie('9-Mab', rel)).toEqual([ 'a', 'b', 1643135400000, 5 ])
+		expect(decodeCookie('!0v018 chars or moreb', rel)).toEqual([ '8 chars or more', 'b', 0, 21 ])
+		expect(decodeCookie('!0v138 chars or moreb', rel)).toEqual([ '8 chars or more', 'b', 1466907912000, 21 ])
+		expect(decodeCookie('!0v808 chars or moreb', rel)).toEqual([ '8 chars or more', 'b', 1488997800000, 21 ])
+		expect(decodeCookie('!0v-M8 chars or moreb', rel)).toEqual([ '8 chars or more', 'b', 1643135400000, 21 ])
+		expect(decodeCookie('!rf018 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', rel)).toEqual([ '8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 0, 128 ])
+		expect(decodeCookie('!rf0J8 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', rel)).toEqual([ '8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 1466898408000, 128 ])
+		expect(decodeCookie('!rfdY8 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', rel)).toEqual([ '8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 1505413800000, 128 ])
+		expect(decodeCookie('!rf-s8 chars or moreA quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', rel)).toEqual([ '8 chars or more', 'A quick brown fox jumps over the lazy dog and what not etc etc etc .. Jackdaws love my big sphinx of quartz.', 1642271400000, 128 ])
+	})
+})
